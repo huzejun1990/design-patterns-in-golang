90ac00103668e63f07e3a42c394df094a2603e49
huzejun <961639130@qq.com>
2024-04-23 16:55:13 +0800

    go语言了解设计模式（23个完整）十七、组合模式 2024-4-23 16:54:55

---
+++ structural/Composite/composite_test.go
@@ -0,0 +1,23 @@
+// @Author huzejun 2024/4/22 21:48:00
+package Composite
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestComposite_Traverse(t *testing.T) {
+	containers := make([]Composite, 4)
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			containers[i].Add(NewLeaf(i*3 + j))
+		}
+	}
+	for i := 1; i < 4; i++ {
+		containers[0].Add(&containers[i])
+	}
+	for i := 0; i < 4; i++ {
+		containers[i].Traverse()
+		fmt.Printf("Finished \n")
+	}
+}
